b23b9e3182ade68f9ad81c2197a38f33b91b6e85
Anjef Dangol <njrnjf@gmail.com>
2026-08-14 23:07:53 +0545

    Fix : next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,21 +9,15 @@ const nextConfig = {
   ],
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.r2.cloudflarestorage.com',
-      },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: '*.r2.cloudflarestorage.com' },
     ],
   },
   async rewrites() {
     return [
       {
         source: '/api/v1/:path*',
-        destination: `${process.env.API_INTERNAL_URL || 'http://localhost:4000/api/v1'}/:path*`,
+        destination: 'https://chiba-api.onrender.com/api/v1/:path*',
       },
     ];
   },
